3c05969f8e008894e73dee00b1e8e656007077d9
AndyVilla <a00829355@itesm.mx>
2021-11-10 13:00:38 -0600

    Cambios Sin Terminar
    Adicion de pantalla de archivos y funcionalidad incompleta

---
+++ AppPrepanet/AppPrepanet/ViewControllerArchivo.swift
@@ -0,0 +1,48 @@
+//
+//  ViewControllerArchivo.swift
+//  AppPrepanet
+//
+//  Created by Jose Andres Villarreal Montemayor on 11/10/21.
+//
+
+import UIKit
+
+class ViewControllerArchivo: UIViewController {
+    
+    
+    init(activityItems: [Any], appActivities: [UIActivity]?){
+        
+    }
+    
+    lazy var arregloDummy : [String] = ["Nombre", "Matricula", "Campus"]
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    let datosUsuario = arregloDummy.joined(separator: ",")
+    let share = [datosUsuario] as [Any]
+    let activityViewController = UIActivityViewController(activityItems: share, applicationActivities: nil)
+    
+    //para iPad
+    //activityViewController.popoverPresentationController?.sourceView = self.view
+    //para iPhone y iPod
+    self.present(activityViewController, animated: true, completion: nil)
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
