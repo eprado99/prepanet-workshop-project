efa433d977654948d138b95c5bb29c56e237bd6e
Hector Guapo <a01197463@itesm.mx>
2021-11-25 23:17:39 -0600

    Arreglo de constrains

--- AppPrepanet/AppPrepanet/ViewControllerPerfil.swift
+++ AppPrepanet/AppPrepanet/ViewControllerPerfil.swift
@@ -47,8 +47,8 @@ class ViewControllerPerfil: UIViewController, UITableViewDataSource, UITableView
         print("USER EN PERFIL \(user.matricula)")
 
         // Styling
-        drawSeparator(xCoor: 10, yCoor: 240)
-        drawSeparator(xCoor: 10, yCoor: 380)
+        drawSeparator(xCoor: 10, yCoor: 280)
+        drawSeparator(xCoor: 10, yCoor: 400)
         
         tableView.reloadData()
         
